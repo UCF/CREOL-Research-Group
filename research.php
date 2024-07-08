34e2daa84776f76c9a581ccfaf7cd440100009f6
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-07-08 20:41:18 +0000

    .

--- research.php
+++ research.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: CREOL Research Groups
+Plugin Name: Research Groups
 Description: Create dropdown modals for 'Research at CREOL' page
 Version: 0.0.0
 Author: UCF Web Communications
