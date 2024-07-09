e0c53ce9272560d43bce2b328429501723bafedc
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-07-09 18:00:29 +0000

    ..

--- includes/research-layout.php
+++ includes/research-layout.php
@@ -33,6 +33,50 @@ function research_display($atts = [], $content = null, $tag = '')
 
     ob_start();
 
+    echo '<style>
+        .section-title {
+            border-bottom: 3px solid #ffcc00;
+        }
+        .custom-card {
+            border: none;
+            border-radius: 10px;
+            background: #ffcc00;
+            display: flex;
+            align-items: center;
+            padding: 15px;
+            margin-bottom: 15px;
+            width: 100%;
+        }
+        .custom-card img {
+            width: 150px;
+            height: 150px;
+            object-fit: cover;
+            border-radius: 50%;
+            margin-right: 20px;
+        }
+        .custom-card .card-body {
+            padding-top: 10px;
+            width: 100%;
+        }
+        .custom-card a {
+            color: #000;
+            text-decoration: none;
+            font-size: 0.85rem;
+            display: block;
+        }
+        .job-title {
+            font-size: 0.85rem;
+            color: #000;
+            margin-top: -1em;
+            margin-bottom: 0.5em;
+            display: block;
+        }
+        .card-title {
+            font-size: 1rem;
+        }
+        
+    </style>';
+
     echo '<div class="research-group">';
 
     if (isset($lab_names[$group])) {
@@ -65,14 +109,12 @@ function research_display($atts = [], $content = null, $tag = '')
         }
 
         if ($query->have_posts()) {
-            echo '<div class="row">';
             while ($query->have_posts()) {
                 $query->the_post();
                 $permalink = get_permalink();
                 $featured_image = get_the_post_thumbnail(get_the_ID(), 'medium');
                 $job_title = get_field('person_jobtitle');
 
-                echo '<div class="card-box col-lg-2 col-md-3 col-sm-4 col-6">';
                 echo '<div class="card custom-card">';
                 echo '<a href="' . esc_url($permalink) . '">';
                 if (!empty($featured_image)) {
@@ -86,9 +128,7 @@ function research_display($atts = [], $content = null, $tag = '')
                 echo '</div>';
                 echo '</a>';
                 echo '</div>';
-                echo '</div>';
             }
-            echo '</div>';
             wp_reset_postdata();
         } else {
             echo '<p>No people found in this </p>';
