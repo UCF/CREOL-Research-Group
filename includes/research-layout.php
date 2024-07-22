ab0d7fb1e66bb87ee0fd36e7030d20bc38d46019
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-07-22 20:57:33 +0000

    ...

--- includes/research-layout.php
+++ includes/research-layout.php
@@ -92,7 +92,7 @@ function research_display($atts = [], $content = null, $tag = '')
             console.log(' . $inverse . ' + " var")
             console.log("Logged")
         </script>';
-        if ($wporgs_atts['inverse'] == '')
+        if ($inverse == '')
             echo '<button class="btn btn-outline-i-primary btn-block" type="button" data-toggle="collapse" data-target="#' . esc_attr($group) . '" aria-expanded="true" aria-controls="collapseExample">' . esc_html($lab_names[$group]) . '</button>';
         else
             echo '<button class="btn btn-outline-primary btn-block" type="button" data-toggle="collapse" data-target="#' . esc_attr($group) . '" aria-expanded="true" aria-controls="collapseExample">' . esc_html($lab_names[$group]) . '</button>';
